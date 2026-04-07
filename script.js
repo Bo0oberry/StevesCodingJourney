8fce4979466944a446802eec924a74a9df472671
Steven Aubrey <bueberry58@gmail.com>
2026-04-07 16:17:26 -0400

    stuf

---
+++ script.js
@@ -0,0 +1,18 @@
+const odenLinks = `
+  <div id="odenLinks">
+    <a href="~/theOdenProject/Oden_Projects/index.html"></a>
+    <a href="~/theOdenProject/Oden_Projects/"></a>
+    <a href="~/theOdenProject/Oden_Projects/"></a>
+    <a href="~/theOdenProject/Oden_Projects/"></a>
+    <a href="~/theOdenProject/Oden_Projects/"></a>
+    <a href="~/theOdenProject/Oden_Projects/"></a>
+    <a href="~/theOdenProject/Oden_Projects/"></a>
+    <a href="~/theOdenProject/Oden_Projects/"></a>
+    <a href="~/theOdenProject/Oden_Projects/"></a>
+    <a href="~/theOdenProject/Oden_Projects/"></a>
+    <a href="~/theOdenProject/Oden_Projects/"></a>
+    <a href="~/theOdenProject/Oden_Projects/"></a>
+    <a href="~/theOdenProject/Oden_Projects/"></a>
+    <a href="~/theOdenProject/Oden_Projects/"></a>
+  </div>
+`;
