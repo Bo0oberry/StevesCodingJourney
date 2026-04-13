e52f3539899a1b00965f28a57e84ab577d93bd59
Steven Aubrey <bueberry58@gmail.com>
2026-04-12 23:23:19 -0400

    Project Recipies

---
+++ theOdinProject/Odin_Projects/HTML_Foundations/Project_Recipies/scrpits.js
@@ -0,0 +1,10 @@
+const imgWidth = document.querySelector('#linkBox img').getBoundingClientRect().width;
+document.querySelector(':root').style.setProperty('--imgSizeWidth', `${imgWidth}px`);
+
+const box = document.querySelector('#linkBox');
+const links = box.querySelectorAll('a');
+const duration = parseFloat(getComputedStyle(document.documentElement).getPropertyValue('--animation-length'));
+
+links.forEach((link, index) => {
+    link.style.animationDelay = `-${(duration / links.length) * index}s`;
+});
