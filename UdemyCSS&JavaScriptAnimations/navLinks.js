8a7bfb19fec77a449a3b1a0debdf6d8fc9f74110
Bo0oberry <bueberry58@gmail.com>
2026-04-17 21:12:45 -0400

    working on udemy transformations

--- UdemyCSS&JavaScriptAnimations/navLinks.js
+++ UdemyCSS&JavaScriptAnimations/navLinks.js
@@ -1,11 +1,6 @@
 const navLinks = ` 
   <ul>
     <li>
-      <a href="/UdemyCSS&JavaScriptAnimations/index.html">
-        Home
-      </a>
-    </li>
-    <li>
       <a href="/UdemyCSS&JavaScriptAnimations/001_cssTransitionOptions/index.html">
         Transition Options
       </a>
@@ -15,10 +10,28 @@ const navLinks = `
         2D Transform Basics
       </a>
     </li>
+    <li>
+      <a href="/UdemyCSS&JavaScriptAnimations/003_transformOrigin/index.html">
+        Transform Origin
+      </a>
+    </li>
+    <li>
+      <a href="/UdemyCSS&JavaScriptAnimations/004_3DTransforms/index.html">
+        3D Transforms
+      </a>
+    </li>
+    <li>
+      <a href="/UdemyCSS&JavaScriptAnimations/005_rotatingButton/index.html">
+        Rotating Button
+      </a>
+    </li>
+    <li>
+      <a href="/UdemyCSS&JavaScriptAnimations/006_swipeButton/index.html">
+        Swipe Button
+      </a>
+    </li>
   </ul>
 `;
-
-
 document.querySelector("nav").innerHTML = navLinks;
 /*  
 <li>
@@ -30,8 +43,7 @@ document.querySelector("nav").innerHTML = navLinks;
 
 const currentLink = window.location.pathname.split("/");
 const currentLinkFolderName = currentLink[currentLink.length - 2];
-
-const navLinkList = document.querySelectorAll('nav ul a');
+const navLinkList = Array.from(document.querySelectorAll('nav a'));
 
 navLinkList.forEach((element,index) => {
   const href = element.getAttribute("href").split('/');
@@ -46,9 +58,77 @@ navLinkList.forEach((element,index) => {
   }
 });
 
-document.querySelector("nav").innerHTML += "<br>"+currentLinkFolderName;
+//document.querySelector("nav").innerHTML+="<br>"+currentLinkFolderName;
 
 
-/*.Header */
+
+/* Header  create structure*/
 const header = document.querySelector('header');
+const active = document.querySelector("nav a.active");
+
+const homeLink = document.createElement("a");
+homeLink.classList.add("headerHome");
+homeLink.href = "/UdemyCSS&JavaScriptAnimations/index.html";
+
+const homeImg = document.createElement('img');
+homeImg.src = "/UdemyCSS&JavaScriptAnimations/forrest_ivysaur_firefly.png";
+homeImg.setAttribute('width', '300');
+homeImg.setAttribute('height', '300');
+homeLink.appendChild(homeImg);
+header.appendChild(homeLink);
+
+
+/* Header  add link src */
+let activeIndex = "home";
+navLinkList.forEach((element,index) => {
+  if(element.classList.contains("active")) {
+    activeIndex = index;
+    return;
+  }
+}); 
+
+switch(activeIndex) {
+  case 'home':
+    header.appendChild(createDiv('headerPrevEmpty'));
+    header.appendChild(createAnchor(navLinkList[0],'headerNext'));
+    break;
+  case 0:
+    const toHome = document.createElement('a');
+    toHome.setAttribute('href', "/UdemyCSS&JavaScriptAnimations/index.html");
+    toHome.innerHTML = 'Home';
+    toHome.classList.add('headerPrev');
+    header.appendChild(toHome);
+    header.appendChild(createAnchor(navLinkList[activeIndex +1],'headerNext'));
+    break;
+  case (navLinkList.length - 1):
+    header.appendChild(createAnchor(navLinkList[activeIndex -1],'headerPrev'));
+    header.appendChild(createDiv('headerNextEmpty'));
+    break;
+  default:
+    header.appendChild(createAnchor(navLinkList[activeIndex -1],'headerPrev'));
+    header.appendChild(createAnchor(navLinkList[activeIndex +1],'headerNext'));
+    break;
+   
+}
+
+function createDiv(className) {
+  const empty = document.createElement('div');
+  empty.classList.add(className);
+  return empty;
+}
+
+function createAnchor(element,className) {
+  const newEle = element.cloneNode(true);
+  newEle.classList.add(className);
+  switch(className) {
+    case "headerPrev":
+      newEle.innerHTML = "prev";
+      break;
+    case "headerNext":
+      newEle.innerHTML = "next";
+      break;
+    }
+  return newEle;
+}
+
 
