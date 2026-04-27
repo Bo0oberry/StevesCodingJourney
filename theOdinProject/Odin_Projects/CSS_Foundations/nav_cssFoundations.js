9a58bb69eb276c95a96d2bed77c68c35feece710
Bo0oberry <bueberry58@gmail.com>
2026-04-27 17:38:58 -0400

    update

--- theOdinProject/Odin_Projects/CSS_Foundations/nav_cssFoundations.js
+++ theOdinProject/Odin_Projects/CSS_Foundations/nav_cssFoundations.js
@@ -8,3 +8,13 @@ fetch('/theOdinProject/Odin_Projects/CSS_Foundations/nav_cssFoundations.html')
     })
     .catch(error => console.error('Error loading navigation HTML: ', error));
 
+
+
+// Close Dropdown menu
+const dropdownButtons = document.querySelectorAll(".dropdown-btn");
+dropdownButtons.forEach((element) => {
+    element.addEventListener('click', (event) => {
+        event.currentTarget.classList.toggle("closed");
+    });
+});
+
