87a20e09db4f3909632b0021d8c870c1007d89f0
Bo0oberry <bueberry58@gmail.com>
2026-07-25 14:03:05 -0400

    update codePen.js

--- theOdinProject/Odin_Projects/CSS_Foundations/The_Cascade/codePen.js
+++ theOdinProject/Odin_Projects/CSS_Foundations/The_Cascade/codePen.js
@@ -2,5 +2,13 @@
 function lineCounter(num) {
   const newSpan = document.createElement('span');
    newSpan.textContent = num;
-   
+}
+
+// manipulate code block 
+function codeBlock() {
+  const codeBlocks = document.querySelectorAll('pre code');
+  codeBlocks.forEach((ele)=>{
+    const lines = ele.innerText.split(/\r?\n/);
+    const lineAmount = lines.length;
+  });
 }
