32fdd5e177574b445ba164a00bcf8dc1d5e1d4c5
Steven Aubrey <bueberry58@gmail.com>
2026-07-26 22:46:10 -0400

    change

--- theOdinProject/Odin_Projects/CSS_Foundations/The_Cascade/codePen.js
+++ theOdinProject/Odin_Projects/CSS_Foundations/The_Cascade/codePen.js
@@ -10,5 +10,12 @@ function codeBlock() {
   codeBlocks.forEach((ele)=>{
     const lines = ele.innerText.split(/\r?\n/);
     const lineAmount = lines.length;
+
+    lines.forEach((ele)=>{
+      const newSpan = document.createElement('span');
+      newSpan.innerHTML = `<span class='lineNum'>${lines.indexOf(ele) + 1}</span>`;
+      newSpan.textContent += ele;
+      ele.parentElement.appendChild(newSpan);
+    });
   });
 }
