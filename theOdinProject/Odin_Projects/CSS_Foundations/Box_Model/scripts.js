e009efffc2dc4b3f769040f97769a7496d5c94c8
Steven Aubrey <bueberry58@gmail.com>
2026-09-07 13:02:00 -0400

    update

--- theOdinProject/Odin_Projects/CSS_Foundations/Box_Model/scripts.js
+++ theOdinProject/Odin_Projects/CSS_Foundations/Box_Model/scripts.js
@@ -1,10 +1,10 @@
 function loadBox() {
   const boxArray = document.querySelectorAll(".header_boxMain");
   const headerHeight = document.querySelector("header").offsetHeight;
-  const boxHeight = headerHeight * 0.8;
-  const translateZValue = boxHeight / 2 + "px";
+  const boxHeight = (headerHeight * 0.8) ;
+  const translateZValue = boxHeight / 2;
 
-  
+
 
   boxArray.forEach((element)=>{
     const boxSidesArray = element.querySelectorAll(".header_boxSide");
@@ -13,19 +13,39 @@ function loadBox() {
       side.style.height= boxHeight + "px";
     });
 
-    //1 front face
-    boxSidesArray[0].style.transform = `translateZ(${translateZValue})`;
+    const translateToMiddle = `translateX(-${translateZValue}px) translateY(-${translateZValue}px)`;
+    
+
+    // 1 front face
+    boxSidesArray[0].style.transform = ` ${translateToMiddle} translateZ(${translateZValue}px) `;
+    boxSidesArray[0].style.backgroundColor = "red";
+
     // 2 back face
-    boxSidesArray[1].style.transform = `translateZ(-${translateZValue})`;
+    boxSidesArray[1].style.transform = ` ${translateToMiddle} translateZ(-${translateZValue}px) `;
+    boxSidesArray[1].style.backgroundColor = "lime";
     // 3 top face
-    boxSidesArray[2].style.transform = `rotateX(90deg) translateZ(${translateZValue})`;
+    boxSidesArray[2].style.transform = ` ${translateToMiddle} rotateX(90deg) translateZ(${translateZValue}px) `;
+    boxSidesArray[2].style.backgroundColor = "yellow";
+
     // 4 bottom face
-    boxSidesArray[3].style.transform = `rotateX(-90deg) translateZ(${translateZValue})`;
+    boxSidesArray[3].style.transform = ` ${translateToMiddle}  rotateX(-90deg) translateZ(${translateZValue}px) `;
+    boxSidesArray[3].style.backgroundColor = "cyan";
+
     // 5 left face
-    boxSidesArray[4].style.transform = `rotateY(-90deg) translateZ(${translateZValue}`;
+    boxSidesArray[4].style.transform = ` ${translateToMiddle} rotateY(-90deg) translateZ(${translateZValue}px)  `;
+    boxSidesArray[4].style.backgroundColor = "hotpink";
+    
     // 6 right face
-    boxSidesArray[5].style.transform = `rotateY(90deg) translateZ(${translateZValue})`;
-  });
+    boxSidesArray[5].style.transform = ` ${translateToMiddle} rotateY(90deg) translateZ(${translateZValue}px) `;
+    boxSidesArray[5].style.backgroundColor = "orange";
+  });;
+  
+}
+
+
+function load_shape(){
+  
 }
 
 loadBox();
+
